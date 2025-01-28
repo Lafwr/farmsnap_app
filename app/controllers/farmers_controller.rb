86e26b5c4a99ed0f623d372ab97f5a2592f0698a
abdoul nagib <abdoulnagib@gmail.com>
2025-01-28 14:24:47 +0000

    Added farmers controller

---
+++ app/controllers/farmers_controller.rb
@@ -0,0 +1,50 @@
+class FarmersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_not_farmer, only: [:new, :create]
+
+  def index
+    @farmers = Farmer.all
+  end
+
+  def show
+    @farmer = Farmer.find(params[:id])
+  end
+
+  def new
+    @farmer = Farmer.new
+  end
+
+  def create
+    @farmer = current_user.build_farmer(farmer_params)
+    if farmer.save
+      redirect_to @farmer, notice: "Welcome Your Farmer Profile Has Been Created."
+    else
+      render :new, status: unprocessable_entity, alert: "ERROR: Farmer Profile Not Created"
+    end
+  end
+
+  def edit
+    @farmer = Farmer.find(params[:id])
+  end
+
+  def update
+    if @farmer.update(farmer_params)
+      redirect_to @farmer, notice: "Your Farmer Profile Has Been Updated."
+    else
+      render :edit, status: unprocessable_entity, alert: "ERROR: Farmer Profile Not Updated"
+    end
+  end
+
+  private
+
+  def farmer_params
+    params.require(:farmer).permit(:bio, :location)
+  end
+
+  def ensure_not_farmer
+    redirect_to root_path, alert: "You already have a farmer profile."
+    if
+      current_user.farmer.present?
+    end
+  end
+end
