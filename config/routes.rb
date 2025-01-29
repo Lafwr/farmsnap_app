216ebb7db5789b6d72c8bf9c2e98cf5c7e234a3a
abdoul nagib <abdoulnagib@gmail.com>
2025-01-29 17:05:54 +0000

    Added cloudinary to the farmer profile form and placeholder styling

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   resources :farmers, exlude: [:destroy]
+  get "my-profile", to: "farmers#myprofile", as: "profile"
 
   resources :events do
     collection do
