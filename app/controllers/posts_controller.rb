be361f94e5c59b0a338041e041d2ecbcecbae16f
Oskar Puzio <osk.vitae@gmail.com>
2025-02-03 16:50:08 +0000

    Posts index stack - basic, no design

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :like, :comment, :destroy]
+  before_action :set_post, only: [:show, :like, :comment, :destroy]
+  before_action :ensure_farmer, only: [:new, :create, :destroy]
+  before_action :authorize_post_deletion, only: [:destroy]
 
   def index
-
+    # DESCENDING ORDER
+    @posts = Post.includes(:farmer, :likes, :comments).order(created_at: :desc)
+    # ADD LIMIT?
+    # Research .includes -- Supposed efficient manner of loading
   end
 
   def show
