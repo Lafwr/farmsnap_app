b92a35e503823e9fa2fe965338ba3bf16eabcf0c
Oskar Puzio <osk.vitae@gmail.com>
2025-02-03 10:20:26 +0000

    Added username,firstname,lastname to devise and sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_29_201701) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_03_095726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -134,6 +134,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_29_201701) do
     t.string "role", default: "standard"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
