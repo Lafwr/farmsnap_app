9f3a600cbd7a7c2deb1a1638dd0b5e5c936cfc9d
abdoul nagib <abdoulnagib@gmail.com>
2025-02-04 18:19:57 +0000

    added new user info to the seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_04_132231) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_04_181609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -163,7 +163,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_132231) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "farmer_id", null: false
+    t.bigint "user_id"
     t.index ["farmer_id"], name: "index_reviews_on_farmer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -200,4 +202,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_04_132231) do
   add_foreign_key "posts", "farmers"
   add_foreign_key "products", "crates"
   add_foreign_key "reviews", "farmers"
+  add_foreign_key "reviews", "users"
 end
