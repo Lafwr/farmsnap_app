24d8a02b4c1a1bae80d2a0f14d2af5d24844f16a
Oskar Puzio <osk.vitae@gmail.com>
2025-02-04 16:45:15 +0000

    posts: custom ruby method added to application helper file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # METHOD USED TO SHOW POST DATE AND TIME
+  def post_timer(timestamp)
+
+    now = Time.zone.now
+
+    if (now - timestamp) < 24.hours
+      timestamp.strftime('%H:%M') # Show only time if less than 24 hours
+    else
+      timestamp.strftime('%d-%Y-%m %H:%M') # Show full date and time
+    end
+  end
 end
