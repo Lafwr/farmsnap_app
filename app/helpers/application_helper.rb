1149e39bc9e552b25bfaaef44aef09de42c7d927
Oskar Puzio <osk.vitae@gmail.com>
2025-02-04 17:17:37 +0000

    Index page for posts - template made

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
     if (now - timestamp) < 24.hours
       timestamp.strftime('%H:%M') # Show only time if less than 24 hours
     else
-      timestamp.strftime('%d-%Y-%m %H:%M') # Show full date and time
+      timestamp.strftime('%d-%m-%Y %H:%M') # Show full date and time
     end
   end
 end
