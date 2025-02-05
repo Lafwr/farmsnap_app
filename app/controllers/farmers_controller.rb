173494aa60f07bb76af69dee29812d672e712013
Jesse Lafour <187882466+Lafwr@users.noreply.github.com>
2025-02-05 14:37:44 +0000

    Pre demo

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -1,5 +1,5 @@
 class FarmersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show]
   before_action :ensure_not_farmer, only: [:new, :create]
 
   def index
